6078cd358e69b578c0be62cbfa9112e6fd14096a
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-02 14:16:15 +0200

    close 2015

--- 2015/03/b.py
+++ 2015/03/b.py
@@ -1,3 +1,5 @@
+# I got the santasTurn idea from Mo Beigi https://mobeigi.com/blog/category/capture-the-flag/advent-of-code/
+# They used it reluctantly but I really liked it.
 with open('input.txt') as input:
     input = input.read()
 
@@ -5,28 +7,27 @@ houses = set()
 
 coordinates = [0, 0]
 robo_coordinates = [0, 0]
+santasTurn = True
 
-# Lol, there is a better way
 for i, ele in enumerate(input):
-    if (i % 2 == 0):
-        if (ele == '>'):
-            coordinates[0] += 1
-        elif (ele == '^'):
-            coordinates[1] += 1
-        elif (ele == '<'):
-            coordinates[0] -= 1
-        elif (ele == 'v'):
-            coordinates[1] -= 1
+    if (ele == '>'):
+        coordinates[0] += int(santasTurn)
+        robo_coordinates[0] += int(not santasTurn)
+    elif (ele == '^'):
+        coordinates[1] += int(santasTurn)
+        robo_coordinates[1] += int(not santasTurn)
+    elif (ele == '<'):
+        coordinates[0] -= int(santasTurn)
+        robo_coordinates[0] -= int(not santasTurn)
+    elif (ele == 'v'):
+        coordinates[1] -= int(santasTurn)
+        robo_coordinates[1] -= int(not santasTurn)
+
+    if (santasTurn):
         houses.update([(tuple(coordinates))])
     else:
-        if (ele == '>'):
-            robo_coordinates[0] += 1
-        elif (ele == '^'):
-            robo_coordinates[1] += 1
-        elif (ele == '<'):
-            robo_coordinates[0] -= 1
-        elif (ele == 'v'):
-            robo_coordinates[1] -= 1
         houses.update([(tuple(robo_coordinates))])
+
+    santasTurn = not santasTurn
 
 print(len(houses))
