4ab11f03ca0016a6f28ee75fb136dc3deab112e5
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-08 12:31:26 +0200

    add .gitignore

--- 2020/java/src/com/adventTools/ReadInput.java
+++ 2020/java/src/com/adventTools/ReadInput.java
@@ -1,8 +1,22 @@
 package com.adventTools;
 
 import java.util.*;
+import java.util.stream.Stream;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class ReadInput {
 
-	
+    public static String inputToString() {
+        StringBuilder sb = new StringBuilder();
+        Path path = Path.of("input.txt");
+        Stream<String> stream = Files.lines(path);
+        try (stream) {
+            stream.forEach(s -> sb.append(s).append("\\n"));
+        } catch (IOException ioe) {
+            System.out.print("Could not stream input");
+        }
+        return sb.toString(); 
+    }
 }
--- 2020/java/src/com/main/Main.java
+++ 2020/java/src/com/main/Main.java
@@ -1,9 +1,12 @@
 package com.main;
 
 import com.days.*;
+import com.adventTools.ReadInput;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+        System.out.println(ReadInput.inputToString());
+        
 	}
+}
