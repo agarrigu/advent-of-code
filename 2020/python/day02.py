58e7f959127adedfa0c7b870320c8c9e92007038
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-09 03:06:20 +0200

    add day02.py

---
+++ 2020/python/day02.py
@@ -0,0 +1,71 @@
+import AdventTools
+import re
+
+
+class Password:
+    def __init__(self, min, max, letter, password):
+        self.min = min
+        self.max = max
+        self.letter = letter
+        self.password = password
+
+    def isValidPassword(self):
+        if (self.min <= self.password.count(self.letter) <= self.max):
+            return True
+        return False
+
+    def isValidPasswordPart2(self):
+        try:
+            indexOne = self.password[self.min - 1]
+            condition1 = indexOne == self.letter
+        except:
+            condition1 = False
+        try:
+            indexTwo = self.password[self.max - 1]
+            condition2 = indexTwo == self.letter
+        except:
+            condition2 = False
+        # TODO There has to be a better way, try again when not drunk XD
+        # Look for method that doesn't care if index out of bound
+        if (condition1 ^ condition2):
+            return True
+        return False
+
+
+def convertStringToPassword(string):
+    min = re.search('^\d+', string).group()
+    max = re.search('-\d+', string).group().strip('-') # bit messy
+    letter = re.search('\s\w{1}:', string).group().strip(': ') # bit messy
+    password = re.search('\w+$', string).group()
+    return Password(int(min), int(max), letter, password)
+
+
+input = AdventTools.ReadInput.inputToStrList()
+
+
+# TODO a() and b() are practically the same, make function
+def a():
+    validPasswords = 0
+    for n in input:
+        if (convertStringToPassword(n).isValidPassword()):
+            validPasswords += 1
+    output = validPasswords
+    return output
+
+
+def b():
+    validPasswords = 0
+    for n in input:
+        if (convertStringToPassword(n).isValidPasswordPart2()):
+            validPasswords += 1
+    output = validPasswords
+    return output
+
+
+def main():
+    print(a())
+    print(b())
+
+
+if __name__ == '__main__':
+    main()
