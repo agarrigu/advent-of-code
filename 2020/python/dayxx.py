25b089cc26c4b8e8a795b902cf9eebc0c31de4b0
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-17 22:56:31 +0200

    dayxx.py template

---
+++ 2020/python/dayxx.py
@@ -0,0 +1,20 @@
+import AdventTools
+
+input = AdventTools.ReadInput.inputToString
+
+
+def a():
+    return 0
+
+
+def b():
+    return 0
+
+
+def main():
+    print(a())
+    print(b())
+
+
+if __name__ == '__main__':
+    main()
