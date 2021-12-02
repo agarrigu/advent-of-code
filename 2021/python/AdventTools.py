938076e94258c7541e244188e40015bfa79f8a83
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-12-02 20:10:53 +0100

    add day02.py and dayxx.py template

---
+++ 2021/python/AdventTools.py
@@ -0,0 +1,41 @@
+from multipledispatch import dispatch
+
+
+class ReadInput:
+
+    @staticmethod
+    def inputToString():
+        with open('input.txt', 'r') as file:
+            input = file.read()
+        return input
+
+    @staticmethod
+    def inputToInt():
+        with open('input.txt', 'r') as file:
+            input = file.read()
+        return int(input)
+
+    @staticmethod
+    def inputToStrList(separator):
+        with open('input.txt', 'r') as file:
+            input = file.read().split(separator)
+        return input
+
+    @staticmethod
+    def inputToIntLIst():
+        with open('input.txt', 'r') as file:
+            input = file.read().splitlines()
+        return [int(i) for i in input]
+
+
+def main():
+#    a = ReadInput.inputToString()
+#    b = ReadInput.inputToStrList()
+#    c = ReadInput.inputToIntLIst()
+#    d = ReadInput.inputToInt()
+    e = ReadInput.inputToStrList('\n\n')
+    print(e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2021/python/day02.py
@@ -0,0 +1,49 @@
+import AdventTools
+
+input = AdventTools.ReadInput.inputToStrList('\n')
+
+
+def convertInputToTable(input):
+    a = [command.split() for command in input[:-1]]
+    b = [(command[0], int(command[1])) for command in a]
+    return b
+
+
+instructions = convertInputToTable(input)
+
+
+def a():
+    xpos = 0
+    ypos = 0
+    for c in instructions:
+        if c[0] == 'forward':
+            xpos += c[1]
+        elif c[0] == 'down':
+            ypos += c[1]
+        elif c[0] == 'up':
+            ypos -= c[1]
+    return xpos * ypos
+
+
+def b():
+    xpos = 0
+    ypos = 0
+    aim = 0
+    for c in instructions:
+        if c[0] == 'down':
+            aim += c[1]
+        elif c[0] == 'up':
+            aim -= c[1]
+        else:
+            xpos += c[1]
+            ypos += c[1] * aim
+    return xpos * ypos
+
+
+def main():
+    print(a())
+    print(b())
+
+
+if __name__ == '__main__':
+    main()
